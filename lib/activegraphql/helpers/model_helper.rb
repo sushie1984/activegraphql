cd1875558c6c1d465001b748352e699e1947fcee
Sascha Burku <sascha.burku@kaeuferportal.de>
2016-10-07 12:59:35 +0200

    Rubocop

--- lib/activegraphql/helpers/model_helper.rb
+++ lib/activegraphql/helpers/model_helper.rb
@@ -1,14 +1,12 @@
 module ActiveGraphQL
   module Helpers
     module ModelHelper
-
       def map_to_s(conditions = {})
         return '' if conditions.blank?
         return conditions if conditions.is_a?(String)
-        return '' if !conditions.is_a?(Hash)
-        conditions.to_a.collect{ |x| x.join(' ')}.join(', ')
+        return '' unless conditions.is_a?(Hash)
+        conditions.to_a.collect { |x| x.join(' ') }.join(', ')
       end
-
     end
   end
 end
