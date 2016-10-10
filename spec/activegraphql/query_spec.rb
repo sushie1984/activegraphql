f80579da2975665f1334c5178f288d5041d60f9b
Sascha Burku <sascha.burku@kaeuferportal.de>
2016-10-10 11:37:26 +0200

    Add specs

--- spec/activegraphql/query_spec.rb
+++ spec/activegraphql/query_spec.rb
@@ -40,120 +40,153 @@ describe ActiveGraphQL::Query do
       { 'data' => { 'someLongActionName' => { 'someExpected' => 'data' } } }
     end
 
-    before do
-      expect(HTTParty)
+    context 'focus config timeout & bearer' do
+
+      before do
+        expect(HTTParty)
         .to receive(:get).with(url, expected_request_options)
         .and_return(response)
-    end
 
-    subject { query.call(*graph) }
-
-    context 'with timeout configured' do
-      let(:expected_request_options) do
-        { query: { query: expected_query_with_params }, timeout: 0.1 }
       end
 
-      let(:config) do
-        { url: url,
-          http: { timeout: 0.1 } }
-      end
+      subject { query.call(*graph) }
 
-      it { is_expected.to eq(some_expected: 'data') }
-    end
+      context 'with timeout configured' do
+        let(:expected_request_options) do
+          { query: { query: expected_query_with_params }, timeout: 0.1 }
+        end
 
-    context 'without timeout configured' do
-      let(:expected_request_options) do
-        { query: { query: expected_query_with_params } }
-      end
+        let(:config) do
+          { url: url,
+            http: { timeout: 0.1 } }
+        end
 
-      context 'with no errors in the response' do
         it { is_expected.to eq(some_expected: 'data') }
+      end
 
-        context 'with locale' do
-          let(:locale) { :en }
+      context 'without timeout configured' do
+        let(:expected_request_options) do
+          { query: { query: expected_query_with_params } }
+        end
 
-          let(:expected_request_options) do
-            { headers: { 'Accept-Language' => locale.to_s },
+        context 'with no errors in the response' do
+          it { is_expected.to eq(some_expected: 'data') }
+
+          context 'with locale' do
+            let(:locale) { :en }
+
+            let(:expected_request_options) do
+              { headers: { 'Accept-Language' => locale.to_s },
               query: { query: expected_query_with_params } }
+            end
+
+            before { query.locale = locale }
+
+            it { is_expected.to eq(some_expected: 'data') }
+          end
+        end
+
+        context 'with errors in the response' do
+          let(:response) do
+            {
+              'errors' => [
+                { 'message' => 'message1' },
+                { 'message' => 'message2' }
+              ]
+            }
           end
 
-          before { query.locale = locale }
+          it 'fails with an error' do
+            expect { subject }.to raise_error(ActiveGraphQL::Query::ServerError,
+            /"message1", "message2"/)
+          end
+        end
+      end
+
+      context 'with bearer auth strategy configured' do
+        let(:token) { 'some.token' }
+
+        let(:expected_request_options) do
+          { query: { query: expected_query_with_params },
+          headers: { 'Authorization' => "Bearer #{token}" } }
+        end
+
+        let(:config) do
+          { url: url,
+            auth: { strategy: :bearer, class: Object } }
+          end
+
+          before do
+            expect(Object).to receive(:encode).and_return(token)
+          end
 
           it { is_expected.to eq(some_expected: 'data') }
         end
-      end
+    end
 
-      context 'with errors in the response' do
-        let(:response) do
-          {
-            'errors' => [
-              { 'message' => 'message1' },
-              { 'message' => 'message2' }
-            ]
-          }
+    context 'focus config http query methods' do
+      context 'method not configured' do
+        before do
+          expect(HTTParty)
+          .to receive(:get).with(url, expected_request_options)
+          .and_return(response)
         end
 
-        it 'fails with an error' do
-          expect { subject }.to raise_error(ActiveGraphQL::Query::ServerError,
-                                            /"message1", "message2"/)
+        let(:expected_request_options) do
+          { query: { query: expected_query_with_params } }
         end
-      end
-    end
 
-    context 'with bearer auth strategy configured' do
-      let(:token) { 'some.token' }
+        let(:config) do
+          { url: url }
+        end
 
-      let(:expected_request_options) do
-        { query: { query: expected_query_with_params },
-          headers: { 'Authorization' => "Bearer #{token}" } }
-      end
+        subject { query.call(*graph) }
 
-      let(:config) do
-        { url: url,
-          auth: { strategy: :bearer, class: Object } }
+        it { is_expected.to eq(some_expected: 'data') }
       end
 
-      before do
-        expect(Object).to receive(:encode).and_return(token)
+      context 'method post configured' do
+        before do
+          expect(HTTParty)
+          .to receive(:post).with(url, expected_request_options)
+          .and_return(response)
+        end
+
+        let(:expected_request_options) do
+          { query: { query: expected_query_with_params } }
+        end
+
+        let(:config) do
+          { url: url, method: :post }
+        end
+
+        subject { query.call(*graph) }
+
+        it { is_expected.to eq(some_expected: 'data') }
       end
 
-      it { is_expected.to eq(some_expected: 'data') }
-    end
-  end
+      context 'method patch configured' do
+        before do
+          expect(HTTParty)
+          .to receive(:get).with(url, expected_request_options)
+          .and_return(response)
 
-  describe '#query_method' do
-    context 'with nil config' do
-      it 'returns :get' do
-        expect(query.query_method).to eq(:get)
-      end
-    end
+          expect do
+            warn('patch is not supported, using default get')
+          end.to output.to_stderr
+        end
 
-    context 'with :post config' do
-      let(:config) do
-        { url: url, method: :post }
-      end
+        let(:expected_request_options) do
+          { query: { query: expected_query_with_params } }
+        end
 
-      it 'returns :post' do
-        expect(query.query_method).to eq(:post)
-      end
-    end
+        let(:config) do
+          { url: url, method: :patch }
+        end
 
-    context 'with :patch config' do
-      let(:config) do
-        { url: url, method: :patch }
-      end
-      let(:warning) do
-        'patch is currently not supported'
-      end
+        subject { query.call(*graph) }
 
-      it 'returns :get' do
-        expect(query.query_method).to eq(:get)
-      end
-
-      it 'displays a warning' do
-        query.query_method
-
-        expect { warn(warning) }.to output.to_stderr
+        it { is_expected.to eq(some_expected: 'data') }
       end
     end
   end
